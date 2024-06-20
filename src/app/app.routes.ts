93fdb177d522495739d0cce8788774770eb186fe
SufiyanAbbasi <115076329+SufiyanAbbasi@users.noreply.github.com>
2024-06-20 18:05:09 +0500

    initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DepartmentComponent } from './pages/department/department.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
+import { ParentcategoryComponent } from './pages/parentcategory/parentcategory.component';
+import { ChildcategoryComponent } from './pages/childcategory/childcategory.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {path: 'dashboard', component:DashboardComponent},
+            {path: 'department', component:DepartmentComponent},
+            {path: 'employee', component:EmployeeComponent},
+            {path: 'parent-category', component:ParentcategoryComponent},
+            {path: 'child-category', component:ChildcategoryComponent},
+            {path: 'employee', component:EmployeeComponent},
+        ]
+
+    }
+];
---
+++ src/app/services/master.service.ts
@@ -0,0 +1,89 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MasterService {
+  
+  apiUrl : string = "https://freeapi.miniprojectideas.com/api/TicketsNew/";
+  constructor(private http: HttpClient) { }
+
+  getAllRoles() {
+    return this.http.get(`${this.apiUrl}GetAllRoles`)
+  }
+
+  login(obj:any){
+    return this.http.post(this.apiUrl + "Login", obj)
+  }
+
+  getAllDept(){
+    return this.http.get(`${this.apiUrl}GetDepartments`)
+  }
+
+  createNewDept(obj:any){
+    return this.http.post(`${this.apiUrl}CreateDepartment`, obj)
+  }
+
+  updateDept(obj:any){
+    return this.http.put(`${this.apiUrl}UpdateDepartment`, obj)
+  }
+
+  deleteDeptById(id:number){
+    return this.http.delete(`${this.apiUrl}DeleteDepartment?id=${id}`)
+  }
+
+
+  getAllpCategory() {
+    return this.http.get(`${this.apiUrl}GetParentCategory`)
+  }
+  
+  createpCategory(obj:any) {
+    return this.http.post(`${this.apiUrl}CreateParentCategory`, obj)
+  }
+ 
+  updatepCategory(obj:any) {
+    return this.http.put(`${this.apiUrl}UpdateParentCategory`,obj)
+  }
+  deletepCategoryById(id: number) {
+    return this.http.delete(`${this.apiUrl}DeleteParentCategory?id=${id}`)
+  }
+
+
+
+  getAllCCategory() {
+    return this.http.get(`${this.apiUrl}GetChildCategory`)
+  }
+ 
+  createCCategory(obj:any) {
+    return this.http.post(`${this.apiUrl}CreateChildCategory`, obj)
+  }
+ 
+  updateCCategory(obj:any) {
+    return this.http.put(`${this.apiUrl}UpdateChildCategory`,obj)
+  }
+ 
+  deleteCCategoryById(id: number) {
+    return this.http.delete(`${this.apiUrl}DeleteChildCategory?id=${id}`)
+  }
+
+
+  getAllEmp() {
+    return this.http.get(`${this.apiUrl}GetEmployees`)
+  }
+ 
+  createEmp(obj:any) {
+    return this.http.post(`${this.apiUrl}CreateEmployee`, obj)
+  }
+ 
+  updateEmp(obj:any) {
+    return this.http.put(`${this.apiUrl}UpdateEmployee`,obj)
+  }
+ 
+  deleteEmpById(id: number) {
+    return this.http.delete(`${this.apiUrl}DeleteEmployee?id=${id}`)
+  }
+
+
+
+}
